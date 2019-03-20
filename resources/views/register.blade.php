5b00dfab8c124dd76ac590c5d19f5b069469b7bd
manhpp <phumanh1998@gmail.com>
2019-03-20 22:40:44 +0700

    thêm trang register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title Page-->
+    <title>Register</title>
+
+    <!-- Icons font CSS-->
+    <link href="{{ URL::asset("css/material-design-iconic-font.min.css") }}" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset("fonts/font-awesome-4.7.0/css/font-awesome.min.css") }}">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <link href="{{ URL::asset("css/select2.min.css") }}" rel="stylesheet" media="all">
+    <link href="{{ URL::asset("css/daterangepicker.css") }}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{ URL::asset("css/main-resgister.css") }}" rel="stylesheet" media="all">
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-02 p-t-130 p-b-100 font-poppins">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-4">
+                <div class="card-body">
+                    <h2 class="title">Registration Form</h2>
+                    <form method="POST">
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">first name</label>
+                                    <input class="input--style-4" type="text" name="first_name">
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">last name</label>
+                                    <input class="input--style-4" type="text" name="last_name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Birthday</label>
+                                    <div class="input-group-icon">
+                                        <input class="input--style-4 js-datepicker" type="text" name="birthday">
+                                        <i class="zmdi zmdi-calendar-note input-icon js-btn-calendar"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Gender</label>
+                                    <div class="p-t-10">
+                                        <label class="radio-container m-r-45">Male
+                                            <input type="radio" checked="checked" name="gender">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                        <label class="radio-container">Female
+                                            <input type="radio" name="gender">
+                                            <span class="checkmark"></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row row-space">
+                            <div class="col-2">
+                                <div class="input-group">
+                                    <label class="label">Email</label>
+                                    <input class="input--style-4" type="email" name="email">
+                                </div>
+                            </div>
+                           
+                        </div>
+                        
+                        <div class="p-t-15">
+                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="{{ URL::asset("js/jquery-3.3.1.min.js") }}"></script>
+    <!-- Vendor JS-->
+    <script src="{{ URL::asset("js/select2.min.js") }}"></script>
+    <script src="{{ URL::asset("js/moment.min.js") }}"></script>
+    <script src="{{ URL::asset("js/daterangepicker.js") }}"></script>
+
+    <!-- Main JS-->
+    <script src="{{ URL::asset("js/global.js") }}"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/login', function(){
     return view('login');
 });
+
+Route::get('/register', function(){
+    return view('register');
+});
