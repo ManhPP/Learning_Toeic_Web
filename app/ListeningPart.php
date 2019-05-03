70c64b47c0169ab81ad17ad80a205f8ad558507f
kiennk <nguyenkien549@gmail.com>
2019-05-04 00:52:38 +0700

    add relationship listeningpart model

--- app/ListeningPart.php
+++ app/ListeningPart.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class ListeningPart extends Model
 {
-    //
+    protected $primaryKey = 'id';
+    protected $table = 'listening_parts';
+
+    public function part1(){
+        return $this->hasMany('App\Part1','idPartNghe','id');
+    }
+
+    public function part2(){
+        return $this->hasMany('App\Part2','idPartNghe','id');
+    }
+
+    public function conversation_paragraph(){
+        return $this->hasMany('App\ConversationParagraph','idPartNghe','id');
+    }
+
+    public function users(){
+        return $this->belongsToMany('App\Account','map_user_listenings','idPartNghe','idAcc')
+            ->withPivot('ngayLam');
+    }
+
+    public function tests(){
+        return $this->belongsToMany('App\Test','map_listening_tests','idPartNghe','idBKT');
+    }
 }
