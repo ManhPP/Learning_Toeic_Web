dc843ad363095ffd8a46ae08ddf0a07fd819c3d5
kiennk <nguyenkien549@gmail.com>
2019-05-05 21:47:24 +0700

    update migration part7paragraph

--- database/migrations/2019_04_24_021423_create_part7_paragraphs_table.php
+++ database/migrations/2019_04_24_021423_create_part7_paragraphs_table.php
@@ -16,7 +16,7 @@ class CreatePart7ParagraphsTable extends Migration
         Schema::create('part7_paragraphs', function (Blueprint $table) {
             $table->integer('id', true);
             $table->string('doanVan1', 512)->nullable(false);
-            $table->string('doanVan2', 512)->nullable(false);
+            $table->string('doanVan2', 512)->nullable(true);
             $table->string('loaiPart7', 64)->nullable(false);
 
             $table->timestamps();
