a9e0a9d7ec290d594003ad1147edc27d2abb1801
kiennk <nguyenkien549@gmail.com>
2019-05-07 22:04:38 +0700

    them phan update part 6

--- app/Http/Controllers/ReadingPartController.php
+++ app/Http/Controllers/ReadingPartController.php
@@ -2,84 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\MapPart6Paragraph;
+use App\Part6Paragraph;
 use App\ReadingPart;
 use Illuminate\Http\Request;
 
 class ReadingPartController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+
+    public function getPart6($id){
+        $partDoc = ReadingPart::find($id);
+        $arrDoan = Part6Paragraph::all();
+        $sum = Part6Paragraph::count();
+        return view('update_part_6',['partDoc'=>$partDoc,'arrDoan'=>$arrDoan,'sum'=>$sum]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function addPart6(Request $request){
+        try{
+            //parse string to part6
+            $part6 = json_decode($request->part6);
+
+            $addPart6 = new ReadingPart();
+            $addPart6->loaiPart = $part6->loaiPart;
+            $addPart6->title = $part6->tittle;
+            $addPart6->accessCount = 0;
+            $row = $addPart6->save();
+            if($row>0){
+                foreach ($part6->listDoanVanPart6 as $doanPart6){
+                    $mapPart6Paragraph = new MapPart6Paragraph();
+                    $mapPart6Paragraph->idDoanVan = $doanPart6->id;
+                    $mapPart6Paragraph->idPartDoc = ReadingPart::max('id');
+                    $mapPart6Paragraph->save();
+                }
+            }
+            return 'true';
+        }catch (\Exception $e){
+            error_log($e->getMessage());
+        }
+        return 'false';
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function updatePart6(Request $request){
+        try{
+            $data = $request->part6;
+            $partDoc_Obj = json_decode($data);
+            $partDoc = ReadingPart::find($partDoc_Obj->id);
+            $partDoc->title = $partDoc_Obj->tittle;
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\ReadingPart  $readingPart
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ReadingPart $readingPart)
-    {
-        //
-    }
+            $arrId = array();
+            foreach ($partDoc_Obj->listDoanVanPart6 as $doanvan){
+                array_push($arrId ,$doanvan->id);
+            }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\ReadingPart  $readingPart
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(ReadingPart $readingPart)
-    {
-        //
+            $partDoc->part6Paragraphs()->sync($arrId);
+            $partDoc->save();
+            return 'true';
+        }catch(\Exception $ex){
+            error_log($ex->getMessage());
+        }
+        return 'false';
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\ReadingPart  $readingPart
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, ReadingPart $readingPart)
-    {
-        //
+    public function practicePart6($id){
+        $part6 = ReadingPart::find($id);
+        return view("part6",['partDoc'=>$part6]);
     }
+    public function practicePart7($id){
+        $part7 = ReadingPart::find($id);
+        return view("part7",['partDoc'=>$part7]);
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\ReadingPart  $readingPart
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(ReadingPart $readingPart)
-    {
-        //
     }
 }
--- app/MapPart6Paragraph.php
+++ app/MapPart6Paragraph.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MapPart6Paragraph extends Model
 {
-    //
+
 }
